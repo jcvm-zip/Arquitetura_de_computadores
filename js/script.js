1241cff3c26988e104cdb01c13ee9a02078e7128
Jefferson Monteiro <zangetsu@pop-os.localdomain>
2023-05-18 15:26:47 -0300

    script de rolagem

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-
+function scrollToSection() {
+    var section = document.getElementById("secao1");
+    section.scrollIntoView({ behavior: "smooth" });
+    
+}
+function scrollToSection2() {
+    var section = document.getElementById("secao2");
+    section.scrollIntoView({ behavior: "smooth" });
+}
